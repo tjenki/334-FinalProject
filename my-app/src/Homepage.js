87c5813f9934159ae9cbbbaeb769e82ad45dd1b8
tjenki <159318296+tjenki@users.noreply.github.com>
2026-04-30 15:37:34 -0400

    adding folders and clearing homepage

---
+++ my-app/src/Homepage.js
@@ -0,0 +1,13 @@
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <header className="Homepage-header">
+        Everyday Tracker App
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
