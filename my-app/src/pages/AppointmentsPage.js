e44991f75850f792bc5116d496dcd4f348793424
angelj165 <angelann165@gmail.com>
2026-05-11 16:21:22 -0400

    added links

--- my-app/src/pages/AppointmentsPage.js
+++ my-app/src/pages/AppointmentsPage.js
@@ -1,3 +1,23 @@
-export default function AppointmentsPage() {
-  return <h2>Appointments Page</h2>;
+import { Link } from "react-router-dom";
+
+function AppointmentsPage() {
+  return (
+    <div>
+      <header className="hero">
+        <div className="hero-top">
+          <div>
+            <h1 className="eyebrow">
+              <Link to="/" className="title-link-medication-page">
+                Appointments
+              </Link>
+            </h1>
+
+
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
+
+export default AppointmentsPage;
