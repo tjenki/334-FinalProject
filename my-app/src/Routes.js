b89493b2707f85718aae3b0e34f7a50f6e411adb
aarna-joshi <aarna@udel.edu>
2026-05-11 21:22:46 -0400

    routes correctly to medications

--- my-app/src/Routes.js
+++ my-app/src/Routes.js
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Homepage from "./Homepage";
+import MedicationsPage from './pages/MedicationsPage';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/home" element={<Homepage />} />
+      <Route path="/medications" element={<MedicationsPage />} />
     </Routes>
   );
 }// import { Routes, Route } from 'react-router-dom';
 // import Homepage from './Homepage';
-// import MedicationsPage from './pages/MedicationsPage';
 // import AppointmentsPage from './pages/AppointmentsPage';
 // import TasksPage from './pages/TasksPage';
 // import LoginPage from "./pages/LoginPage";
